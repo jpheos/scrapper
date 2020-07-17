163d53f9dcc7215a6a835944eabbac7eb1dd3025
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-07-17 11:22:30 +0200

    fix devise oauth google

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,7 +26,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
