1f08da8807995cbf10e7da72adaede45951a5a00
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-18 12:05:11 +0200

    FetchService créé close #5

--- lib/scrapper/engine.rb
+++ lib/scrapper/engine.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
+require_relative "./selector.rb"
 
-require 'open-uri'
-require 'uri'
+require "open-uri"
+require "uri"
 
 class Scrapper::Engine
   def initialize(json:)
-    @start_url = json['startUrl'].first
+    @start_url = json["startUrl"].first
     extract_domain
-    @selectors = json['selectors'].map { |s| Scrapper::Selector.from_json(s, @domain) }
+    @selectors = json["selectors"].map { |s| Scrapper::Selector.from_json(s, @domain) }
     generate_sitemap
   end
 
   def debug_selector(selector, i = 0) # rubocop:disable Naming/MethodParameterName
-    print '-' * i
+    print "-" * i
     ap selector.id
     selector.children.each do |s|
       debug_selector(s, i + 2)
@@ -21,9 +22,10 @@ class Scrapper::Engine
 
   def call
     doc = Nokogiri::HTML(URI.open(@start_url))
-    first_selector = @selectors.find { |s| s.parent_selector == '_root' }
+    first_selector = @selectors.find { |s| s.parent_selector == "_root" }
     # debug_selector(first_selector)
-    ap first_selector.data(doc)
+    # ap first_selector.data(doc)
+    first_selector.data(doc)
   end
 
   def generate_sitemap
