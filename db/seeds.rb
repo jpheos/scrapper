195e940a3926986c0ab8b1abba5ac1a9d51b051a
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-22 23:03:37 +0200

    Petite modif sur le seed et dans les json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,30 @@
 Item.destroy_all
 JsonEntry.destroy_all
+Area.destroy_all
+User.destroy_all
 
+ap "Creation User"
+user = User.create!(email: "inicolas69@gmail.com", password: "azerty")
+ap user
 
-if User.first
-  user = User.first
-else
-  user = User.create!(email: "jooo.blanchard@gmail.com", password: "azerty")
-end
+ap "Creation Area"
 area = Area.create!(user: user, name: "Test Lyon")
+ap area
+
+ap "Creation JsonEntry"
 json_entries = [
   {
     name: "Espaces atypiques",
     area: area,
-    data: open("data/json_files/espaces_atypiques.json").read
+    data: open("data/json_files/espaces_atypiques.json").read,
   },
   {
     name: "Selection Immo",
     area: area,
-    data: open("data/json_files/selection_immo.json").read
-  }
+    data: open("data/json_files/selection_immo.json").read,
+  },
 ]
 json_entries.each do |attr|
-  JsonEntry.create!(attr)
+  json_entry = JsonEntry.create!(attr)
+  ap json_entry
 end
-
