c1553c87551f43d6853d5728ee8c37d1ee6be454
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-22 23:02:30 +0200

    Ajout route Json_entry#Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get  'pushbullet/callback'
-  get  'pushbullet/init'
-  get  'pushbullet/devices'
-  post 'pushbullet/devices/pick', to: 'pushbullet#devices_pick'
+  get "pushbullet/callback"
+  get "pushbullet/init"
+  get "pushbullet/devices"
+  post "pushbullet/devices/pick", to: "pushbullet#devices_pick"
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :areas do
     resources :json_entries, only: %i[index new create]
   end
 
-  resources :json_entries, only: %i[edit update destroy]
+  resources :json_entries, only: %i[edit update destroy show]
 
-  root to: 'areas#index'
+  root to: "areas#index"
 end
