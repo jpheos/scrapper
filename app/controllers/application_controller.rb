c81ac60159807b110926f44027a01b5c0366a42f
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-23 13:40:45 +0200

    Ajout des onglets correspondant aux areas dans la navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_areas
 
   def home
     render json: { user: current_user, settings: current_user.pushbullet_setting }
+  end
+
+  private
+
+  def set_areas
+    @areas_navbar = current_user.areas if current_user
   end
 end
